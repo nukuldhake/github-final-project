054865dee0d9f6f804eee50267a11916b355c025
nukuldhake <nukuldhake05@gmail.com>
2026-04-21 14:28:52 +0530

    Complete Final Project: Online Bookstore API

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,105 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+const axios = require('axios');
+
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let results = [];
+  for (let id in books) {
+    if (books[id].author === author) {
+      results.push(books[id]);
+    }
+  }
+  res.send(results);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let results = [];
+    for (let id in books) {
+      if (books[id].title === title) {
+        results.push(books[id]);
+      }
+    }
+    res.send(results);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
+});
+
+// Task 10: Get all books using async/await
+public_users.get('/async-get-all-books', async function (req, res) {
+    try {
+        const response = await axios.get('http://localhost:5000/');
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).json({message: "Error fetching books", error: error.message});
+    }
+});
+
+// Task 11: Get book details based on ISBN using async/await
+public_users.get('/async-get-book-isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = req.params.isbn;
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).json({message: "Error fetching book by ISBN", error: error.message});
+    }
+});
+
+// Task 12: Get book details based on author using async/await
+public_users.get('/async-get-book-author/:author', async function (req, res) {
+    try {
+        const author = req.params.author;
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).json({message: "Error fetching book by author", error: error.message});
+    }
+});
+
+// Task 13: Get book details based on title using async/await
+public_users.get('/async-get-book-title/:title', async function (req, res) {
+    try {
+        const title = req.params.title;
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).json({message: "Error fetching book by title", error: error.message});
+    }
 });
 
 module.exports.general = public_users;
